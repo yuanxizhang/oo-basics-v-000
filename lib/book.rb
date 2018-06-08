017135834684b164102d1abe8a3dd8d131864d74
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-08 18:26:08 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
+  @@genre = []
+  attr_reader :title, :author, :pages
 
+  def initailize(title)
+    @title = title
+    @@gebre << self
+  end
+  
+  def next_page
+    puts "Turn to next page" 
+  end
+end
 
