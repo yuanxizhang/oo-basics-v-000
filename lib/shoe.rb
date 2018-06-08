29e03c135f1ecc615e4b9c7b885a0752ec870cdf
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-08 18:43:40 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,12 @@
 # Make a shoe class
 class Shoe 
-  attr_reader :color, :size
+  attr_reader :color, :size, :material, :condition
+  attr_reader :brand
   
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def cobble
+    puts 
 end
